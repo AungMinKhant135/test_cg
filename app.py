15e09a0b95a8980d706cce1d5bed7fb45e0b1284
AungMinKhant135 <97160958+AungMinKhant135@users.noreply.github.com>
2022-08-27 13:09:01 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import os
-import numpy as np
-import cv2
-from tensorflow.keras.models import load_model
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
+from pycaret.regression import *
+import pandas as pd
 
 
 st.title('Charge&Guess')
@@ -16,9 +13,9 @@ ok = st.button("Calculate Salary")
 #model = load_model('CV_RF_Regression')
 #model = models.load_model('CV_RF_Regression.h5')
 #model =load_model('CV_RF_Regression.h5')
-model =load_model('cg_model.pkl')
+#model =load_model('cg_model.pkl')
 #model = load_model('cnn.h5')
-
+model = load_model('cg_model')
 
 age = 20
 sex = 0
@@ -37,6 +34,6 @@ if ok:
     st.write(features_df)
     #charges = reg_model.predict(data_pre)
     #charges = model_file.predict(features_df)
-    
+    #salary = predict_model(model, data=features_df)
     charges = predict_model(model, data=features_df)
     st.write(charges)
