1bb3a12983f4ca4b4fbcf8e17ddc3d55e38715b5
AungMinKhant135 <97160958+AungMinKhant135@users.noreply.github.com>
2022-08-27 11:10:59 +0630

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from pycaret.regression import *
+import pandas as pd
+
+st.title('Charge&Guess')
+st.markdown('''
+Please use input data
+''')
+
+ok = st.button("Calculate Salary")
+
+model = load_model('CV_RF_Regression')
+
+age = 20
+sex = 0
+bmi = 25.2
+children = 3 
+smoker = 0
+region = 2
+
+features ={'age': age, 'sex': sex ,
+           'bmi': bmi, 'children': children,
+           'smoker': smoker, 'region': region}
+features_df  = pd.DataFrame([features])
+
+if ok:
+    #st.write([age,sex,bmi,children,smoker,region])
+    st.write(features_df)
+    #charges = reg_model.predict(data_pre)
+    #charges = model_file.predict(features_df)
+    
+    charges = predict_model(model, data=features_df)
+    st.write(charges)
